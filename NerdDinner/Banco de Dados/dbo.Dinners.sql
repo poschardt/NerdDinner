a25c1dd4c33763f02959c4dcaccf58b1b6678e1a
poschardt <caio_poschardt@hotmail.com>
2019-05-08 00:00:44 -0300

    Criação do banco de dados

---
+++ NerdDinner/Banco de Dados/dbo.Dinners.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[DinnerID] INT NOT NULL PRIMARY KEY, 
+    [Title] NCHAR(50) NOT NULL, 
+    [EventDate] DATETIME NOT NULL, 
+    [Description] NCHAR(256) NULL, 
+    [HostedBy] NCHAR(20) NULL, 
+    [ContactPhone] NCHAR(20) NULL, 
+    [Address] NCHAR(50) NULL, 
+    [Country] NCHAR(30) NULL, 
+    [Latitude] FLOAT NULL, 
+    [Longitude] FLOAT NULL
+)

60c2ace13b6a3516bf8d4676d951bde117e47914
poschardt <caio_poschardt@hotmail.com>
2019-05-13 16:13:03 -0300

    Validações de erros
    esse commit está um pouco bugado. apresenta erros que não sei corrigir

---
+++ NerdDinner/NerdDinner/Models/DinnerRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NerdDinner.Models
+{
+    public class DinnerRepository
+    {
+        private NerdDinnerDataContext db = new NerdDinnerDataContext();
+
+        //
+        // Query Methods
+
+        public IQueryable<Dinners> FindAllDinners()
+        {
+            return db.Dinners;
+        }
+
+        public IQueryable<Dinners> FindUpcomingDinners()
+        {
+            return from dinner in db.Dinners
+                   where dinner.EventDate > DateTime.Now
+                   orderby dinner.EventDate
+                   select dinner;
+        }
+
+        public Dinners GetDinner(int id)
+        {
+            return db.Dinners.SingleOrDefault(d => d.DinnerID == id);
+        }
+
+        //
+        // Insert/Delete Methods
+
+        public void Add(Dinners dinner)
+        {
+            db.Dinners.InsertOnSubmit(dinner);
+        }
+
+        public void Delete(Dinners dinner)
+        {
+            db.Rsvp.DeleteAllOnSubmit(dinner.Rsvp);
+            db.Dinners.DeleteOnSubmit(dinner);
+        }
+
+        //
+        // Persistence 
+
+        public void Save()
+        {
+            db.SubmitChanges();
+        }
+    }
+}
